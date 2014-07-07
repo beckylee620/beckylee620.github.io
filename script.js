f668159071dc718c6152739535cab5dec52453a2
Becky <msbeckylee@gmail.com>
2014-07-07 15:47:32 +0100

    getting back around to this

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+                  $("div").click(function(){
+                                 $("div").css("background-color", "#000000");
+                                 });
+                  
+                  
+                  });
