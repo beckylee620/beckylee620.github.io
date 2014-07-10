1f4b13e98c8cfc6ad9f6306c8ba899a06496d0d1
Becky <msbeckylee@gmail.com>
2014-07-10 15:54:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
-                  $("div").click(function(){
-                                 $("div").css("background-color", "#000000");
-                                 });
+                  
                   
                   
                   });
