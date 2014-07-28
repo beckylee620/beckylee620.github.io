76f60ed0f37e35da5e047e2f79c02c53416bb3e6
Becky <msbeckylee@gmail.com>
2014-07-28 15:31:26 +0100

    Update c0unting.js

--- c0unting.js
+++ c0unting.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
    $('div').click(function() {
-      $(this).fadeOut('slow')
-/*       $(this).css({
-           'background-color':'#FAFBFC'*/
+       $(this).css({
+           'background-color':'black'
        });
    });
