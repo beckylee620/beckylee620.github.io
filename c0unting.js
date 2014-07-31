7981f177c504c018931af3ad1a80148d7f2b0f15
Becky <msbeckylee@gmail.com>
2014-07-31 12:22:25 +0100

    Update c0unting.js

--- c0unting.js
+++ c0unting.js
@@ -1,8 +1,10 @@
-$(document).ready(function() {
-$('div').click(function() {
-    $(this).css({
-        'background-color':'black'
-        'color':'black'
-    });
-});
-});
+confirm('Please click "OK" if this this is working.');
+
+//$(document).ready(function() {
+//$('div').mouseenter(function() {
+//    $(this).css({
+//        'background-color':'black'
+//        'color':'black'
+//    });
+//});
+//});
