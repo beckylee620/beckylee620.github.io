4ae96fcb43962b0287add8c1924cbdbb647809a3
Becky <msbeckylee@gmail.com>
2014-08-03 22:46:03 +0100

    Update c0unting.js

--- c0unting.js
+++ c0unting.js
@@ -1,10 +1,10 @@
 //confirm('Please click "OK" if this is working.');
-
-$(document).ready(function() {
-  $('div').mouseenter(function() {
-      $(this).css({
-          'background-color':'black'
-          'color':'black'
-      });
-  });
+$( document ).ready(function() {
+ 
+    $( "div" ).click(function( event ) {
+ 
+        alert( "Thanks for visiting!" );
+ 
+    });
+ 
 });
