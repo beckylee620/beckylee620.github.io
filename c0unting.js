2f132c711a47a286267cc77ec33a0f26f2dcf000
Becky <msbeckylee@gmail.com>
2014-08-04 15:31:54 +0100

    Update c0unting.js

--- c0unting.js
+++ c0unting.js
@@ -1,15 +1,18 @@
-$(document).ready(function() {
-
 //create list of all numbers used for this exercise
-  var numbers = _.range(-999, 999);
+var numbers = _.range(-999, 999);
 //  for(var num = -999; num < 1000; num++) {
 //    numbers.push(num);
 //  }
 
 //populate the table with the current numbers
-  var middle = numbers[999];
+var middle = numbers[999];
   
-  document.getElementById("zero").textContent = middle.toString();
+document.getElementById("zero").textContent = middle.toString();
+
+
+
+$(document).ready(function() {
+
   
   
 //  $("div").click(function() {
