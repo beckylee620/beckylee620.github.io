5017b34400b0a44238c705678aa3a2a6b947bd93
Becky <msbeckylee@gmail.com>
2014-08-04 15:40:40 +0100

    Update c0unting.js

--- c0unting.js
+++ c0unting.js
@@ -1,17 +1,25 @@
-document.getElementById("negFive").textContent="-5"
+var negFive = -8;
+
+
+document.getElementById("negFive").textContent=negFive.toString();
+document.getElementById("negFour").textContent="-5";
+document.getElementById("negThree").textContent="-5";
+document.getElementById("negTwo").textContent="-5";
+document.getElementById("negOne").textContent="-5";
+document.getElementById("zero").textContent="-5";
+document.getElementById("posOne").textContent="-5";
+document.getElementById("posTwo").textContent="-5";
+document.getElementById("posThree").textContent="-5";
+document.getElementById("posFour").textContent="-5";
+document.getElementById("posFive").textContent="-5";
 
 
 
-//create list of all numbers used for this exercise
-var numbers = _.range(-999, 999);
 //  for(var num = -999; num < 1000; num++) {
 //    numbers.push(num);
 //  }
 
 //populate the table with the current numbers
-var middle = numbers[999];
-  
-document.getElementById("zero").textContent = numbers[999].toString();
 
 
 
