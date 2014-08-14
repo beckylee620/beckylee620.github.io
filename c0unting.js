7740c5a5052791ac09cb958c8072cc34e4760c11
Becky <msbeckylee@gmail.com>
2014-08-14 10:20:36 +0100

    Update c0unting.js

--- c0unting.js
+++ c0unting.js
@@ -30,39 +30,6 @@ $(document).ready(function() {
 
   $(document).keydown(function(e) {
     switch(e.which) {
-        case 39: // right
-        
-        $("td").animate({paddingRight:"+=10%"}, 100);
-        
-        negFive += 1;
-        negFour += 1;
-        negThree += 1;
-        negTwo += 1;
-        negOne += 1;
-        zero += 1;
-        posOne += 1;
-        posTwo += 1;
-        posThree += 1;
-        posFour += 1;
-        posFive += 1;
-        
-        document.getElementById("negFive").textContent=negFive.toString();
-        document.getElementById("negFour").textContent=negFour.toString();
-        document.getElementById("negThree").textContent=negThree.toString();
-        document.getElementById("negTwo").textContent=negTwo.toString();
-        document.getElementById("negOne").textContent=negOne.toString();
-        document.getElementById("zero").textContent=zero.toString();
-        document.getElementById("posOne").textContent=posOne.toString();
-        document.getElementById("posTwo").textContent=posTwo.toString();
-        document.getElementById("posThree").textContent=posThree.toString();
-        document.getElementById("posFour").textContent=posFour.toString();
-        document.getElementById("posFive").textContent=posFive.toString();
-        
-        $("td").css("padding-left", "-=100%");
-        $("td").css("padding-right", "-=100%");
-
-        break;
-
         
         case 37: // left
         
@@ -96,6 +63,41 @@ $(document).ready(function() {
         $("td").css("padding-left", "-=100%");
         
         break;
+        
+                case 39: // right
+        
+        $("td").animate({paddingRight:"+=10%"}, 100);
+        
+        negFive += 1;
+        negFour += 1;
+        negThree += 1;
+        negTwo += 1;
+        negOne += 1;
+        zero += 1;
+        posOne += 1;
+        posTwo += 1;
+        posThree += 1;
+        posFour += 1;
+        posFive += 1;
+        
+        document.getElementById("negFive").textContent=negFive.toString();
+        document.getElementById("negFour").textContent=negFour.toString();
+        document.getElementById("negThree").textContent=negThree.toString();
+        document.getElementById("negTwo").textContent=negTwo.toString();
+        document.getElementById("negOne").textContent=negOne.toString();
+        document.getElementById("zero").textContent=zero.toString();
+        document.getElementById("posOne").textContent=posOne.toString();
+        document.getElementById("posTwo").textContent=posTwo.toString();
+        document.getElementById("posThree").textContent=posThree.toString();
+        document.getElementById("posFour").textContent=posFour.toString();
+        document.getElementById("posFive").textContent=posFive.toString();
+        
+        $("td").css("padding-left", "-=100%");
+        $("td").css("padding-right", "-=100%");
+
+        break;
+
+
       }
      e.preventDefault(); // prevent the default action (scroll / move caret)
   });
