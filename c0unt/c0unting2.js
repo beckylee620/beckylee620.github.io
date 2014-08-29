5fe658cdf776a9e7de92db386f8a0502fb1a277e
Becky <msbeckylee@gmail.com>
2014-08-29 13:14:00 +0100

    Update c0unting2.js

--- c0unt/c0unting2.js
+++ c0unt/c0unting2.js
@@ -18,7 +18,7 @@ while (integerStart < 1005) {
 
 stageSetting = stageSetting.concat("</tr>");
 
-$(".stageNumbers").css("width", (Math.round(hunWidth/3.7)).toString());
+$(".stageNumbers").css("width", (Math.round(hunWidth)).toString());
 
 $("#stage").html(stageSetting);
 
